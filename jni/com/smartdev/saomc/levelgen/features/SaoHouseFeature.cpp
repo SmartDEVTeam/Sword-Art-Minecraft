984a10c77ccfcd33bc96b379c8123ab1ce957372
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2016-04-02 19:36:28 -0500

    extra

--- jni/com/smartdev/saomc/levelgen/features/SaoHouseFeature.cpp
+++ jni/com/smartdev/saomc/levelgen/features/SaoHouseFeature.cpp
@@ -13,6 +13,10 @@ SaoHouseFeature::SaoHouseFeature() : Feature(false) {
 
 bool SaoHouseFeature::place(BlockSource& region, const BlockPos& pos, Random& rand) const {
 	if(rand.genrand_int32() % 30 == 1) {
+		for(int x = -3; x <= 3; x++) {
+			for(int z = -3; z <= 3; z++) {
+				for(int y = 0; y <= 6; y++) {	
+					if(y == 0 || y == 6 || x == -3 || x == 3 || z == -3 || z == 3)
 						_setBlockAndData(region, {pos.x + x, pos.y + y, pos.z + z}, {CardinalBlocks::cardinalOre->blockId, 0});
 					else
 						_setBlockAndData(region, {pos.x + x, pos.y + y, pos.z + z}, {0, 0});
