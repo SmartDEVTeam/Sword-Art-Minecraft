39a25b2f05645b493f91e9fb537d9782a44357a2
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2016-04-05 21:44:36 -0500

    quick fix

--- jni/com/mojang/minecraftpe/client/renderer/entity/HumanoidMobRenderer.h
+++ jni/com/mojang/minecraftpe/client/renderer/entity/HumanoidMobRenderer.h
@@ -13,6 +13,7 @@ public:
 	HumanoidModel& armorModel1; // 184
 	HumanoidModel& armorModel2; // 188
 	std::vector<mce::TexturePtr> armorTextures; // 192
+	char hmr_vars[8]; // 196
 
 	HumanoidMobRenderer(std::unique_ptr<HumanoidModel>, std::unique_ptr<HumanoidModel>, std::unique_ptr<HumanoidModel>, mce::TexturePtr, float);
 	HumanoidMobRenderer(std::unique_ptr<HumanoidModel>, std::unique_ptr<HumanoidModel>, std::unique_ptr<HumanoidModel>, std::unique_ptr<HumanoidModel>, mce::TexturePtr, float);
